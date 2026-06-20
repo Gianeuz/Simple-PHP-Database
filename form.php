a9704e7eec2045b313ab80a2efa624a75031298a
Gian Ticaya <93826533+Gianeuz@users.noreply.github.com>
2026-06-20 23:36:48 +0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <title>Php Sample</title>
+</head>
+
+
+<body>
+    <center>
+        <h2>Student Form</h2>
+        <form method="post" action="form_action.php">
+            Student Number <input type="text" name="studnum"><br>
+            First Name <input type="text" name="fname"><br>
+            Last Name <input type="text" name="lname"><br>
+            Course <input type="text" name="course"><br>
+            <input type="submit" value="Submit">
+        </form>
+    </center>
+</body>
+
+</html>
